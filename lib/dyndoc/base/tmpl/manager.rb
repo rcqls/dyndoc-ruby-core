b3a1395500dd954ebf68bd0df0a8a3d711d0ea5c
R cqls <rdrouilh@gmail.com>
2014-10-31 11:11:35 +0100

    Allows conemu exec

--- lib/dyndoc/base/tmpl/manager.rb
+++ lib/dyndoc/base/tmpl/manager.rb
@@ -45,7 +45,8 @@ module Dyndoc
       ## ruby and R init for dynArray stuff
       require "dyndoc/common/dynArray"
       dyndocToolsR = File.join(Dyndoc.cfg_dir[:gem_path],"share","R","dyndocTools.R").gsub('\\','/')
-      dyndocToolsR =  ENV['WD'].split('\\')[0...-2].join(File::Separator) + dyndocToolsR  if RUBY_PLATFORM =~ /msys/
+      ## WD is introduced by Msys and ConEmuDir by ConEmu
+      dyndocToolsR =  (ENV['WD'] || ENV['ConEmuDir']).split('\\')[0...-2].join(File::Separator) + dyndocToolsR  if RUBY_PLATFORM =~ /msys/
       R4rb << "source('"+dyndocToolsR+"',chdir=TRUE)"
     end
 
