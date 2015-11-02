443eac80cb1dbdf22d351f50250976069996df90
R cqls <rdrouilh@gmail.com>
2015-11-02 16:46:55 +0100

    md2reveals added

--- lib/dyndoc/common/init.rb
+++ lib/dyndoc/common/init.rb
@@ -1,2 +1,2 @@
 ## CONSTANT to be defined first
-PANDOC_CMDS=["--md2odt","--md2docx","--md2beamer","--md2dzslides","--md2slidy","--md2s5","--md2slidy","--tex2odt","--tex2docx"]
+PANDOC_CMDS=["--md2odt","--md2docx","--md2beamer","--md2dzslides","--md2slidy","--md2s5","--md2slidy","--md2revealjs","--tex2odt","--tex2docx"]
